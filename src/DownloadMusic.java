90813e77f7e374349dca3ee09f8634a5448a3b95
lks <luka.andre0112@gmail.com>
2019-11-22 08:48:35 +0300

    Проверка

--- src/DownloadMusic.java
+++ src/DownloadMusic.java
@@ -1,5 +1,36 @@
-public class DownloadMusic {
-    DownloadMusic(){
+import java.io.*;
+import java.net.URL;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
 
+public class DownloadMusic {
+    private static final String SITE_URL = "src\\inputFile.txt";
+    private static final String URL_FOR_DOWNLOAD_MUSIC = "src\\outputFile.txt";
+
+    DownloadMusic() {
+        try (BufferedReader musicFile = new BufferedReader(new FileReader(SITE_URL))) {
+            String music;
+            int count = 0;
+            try {
+                while ((music = musicFile.readLine()) != null) {
+                    downloadMusic(music, URL_FOR_DOWNLOAD_MUSIC + String.valueOf(count) + ".mp3");
+                    count++;
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private static void downloadMusic(String strUrl, String file) throws IOException {
+        URL url = new URL(strUrl);
+        ReadableByteChannel byteChannel = Channels.newChannel(url.openStream());
+        FileOutputStream stream = new FileOutputStream(file);
+        stream.getChannel().transferFrom(byteChannel, 0, Long.MAX_VALUE);
+        stream.close();
+        byteChannel.close();
     }
 }
