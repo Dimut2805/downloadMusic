d95f2e6cc3edf695e8e84058a81d47830d53a56a
lks <luka.andre0112@gmail.com>
2019-11-26 12:27:20 +0300

    Удалил класс DownloadMusic и разделил его на классы Song и Image Song - методы downloadMusic и playSong Image - методы downloadImage
    В Constants добавил PATH_TO_MUSIC, PATH_TO_IMAGE

---
+++ src/Image.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
+
+public class Image implements Constains {
+
+    public void downloadImage() {
+        try (BufferedReader imageFile = new BufferedReader(new FileReader(DOWNLOAD_URL))) {
+            String image;
+            int count = 0;
+            try {
+                while ((image = imageFile.readLine()) != null) {
+                    download(image, PATH_TO_IMAGE + String.valueOf(count) + ".jpg");
+                    count++;
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+    private static void download(String strUrl, String file) throws IOException {
+        URL url = new URL(strUrl);
+        ReadableByteChannel byteChannel = Channels.newChannel(url.openStream());
+        FileOutputStream stream = new FileOutputStream(file);
+        stream.getChannel().transferFrom(byteChannel, 0, Long.MAX_VALUE);
+        stream.close();
+        byteChannel.close();
+    }
+
+}
