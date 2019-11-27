067454948c7936b4445d47d23b98041b89ae0235
Dimut <Dimut2805@yandex.ru>
2019-11-27 16:34:47 +0300

    Изменены разделы

--- src/ru/pochemuchki/musicproject/Constains.java
+++ src/ru/pochemuchki/musicproject/Constains.java
@@ -8,7 +8,7 @@ import java.util.HashMap;
 
 public interface Constains {
     String DOWNLOAD_URL = "src\\downloadUrl.txt";
-    String PATH_TO_MUSIC = "C:\\Users\\" +System.getProperty("user.name") + "\\Music\\"; // for use file in this directory
+    String PATH_TO_MUSIC = "C:\\Users\\" + System.getProperty("user.name") + "\\Music\\"; // for use file in this directory
     String PATH_TO_IMAGE = "srs\\image";
     String PATH_MUSICS = "C:\\Users\\" + System.getProperty("user.name") + "\\Music";
 
@@ -17,15 +17,19 @@ public interface Constains {
         {
             put("Не выбрано", null);
             put("Топ-100", "https://muzika.vip");
-            put("Русский поп", "https://muzika.vip/m-genres/русский-поп-7071");
-            put("Русский рок", "https://muzika.vip/m-genres/русский-рок-7070");
-            put("Дабстэп", "https://muzika.vip/m-genres/dubstep-7043");
+            put("Billie Eilish", "https://muzika.vip/m-artists/billie-eilish-121989");
+            put("Григорий Лепс", "https://muzika.vip/m-artists/григорий-лепс-6298");
+            put("Группа Кино", "https://muzika.vip/m-artists/виктор-цой-6257");
+            put("Макс Корж", "https://muzika.vip/m-artists/макс-корж-9229");
+            put("Lindsey Stirling", "https://muzika.vip/m-artists/lindsey-stirling-10942");
         }
     };
     ObservableList<String> OBSERVABLE_LIST_SITE_TABS = FXCollections.observableArrayList(
             "Не выбрано",
             "Топ-100",
-            "Русский поп",
-            "Русский рок",
-            "Дабстэп");
+            "Billie Eilish",
+            "Григорий Лепс",
+            "Группа Кино",
+            "Макс Корж",
+            "Lindsey Stirling");
 }
