bf11a1ccf7a4c21ff277653bdf187676ac56d559
Dimut <Dimut2805@yandex.ru>
2019-11-29 15:31:31 +0300

    Произведен сбор мусора

--- src/ru/pochemuchki/musicproject/Launcher.java
+++ src/ru/pochemuchki/musicproject/Launcher.java
@@ -1,16 +1,7 @@
 package ru.pochemuchki.musicproject;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.stage.Stage;
-
-import javafx.application.Application;
-import javafx.stage.Stage;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 
 public class Launcher extends Application implements Constains {
 
