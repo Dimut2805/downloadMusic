5324c0fe1249e07fc031e0aed737d20dc2ece309
lks <luka.andre0112@gmail.com>
2019-11-30 10:22:43 +0300

    Немного рефакторинга и опртимизации для скачивания музыки, добавленно воспроизведение музыки, добавленно скачивание картинки

---
+++ src/ru/pochemuchki/musicproject/Image.java
@@ -0,0 +1,45 @@
+package ru.pochemuchki.musicproject;
+
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * Класс для скачивания картинки
+ */
+public class Image implements Constains {
+
+    /**
+     * Методля для скачивания кртинки который принимает ссылку на картинкуи и имя файла
+     * @param src - ссылка на картинку
+     * @param nameImage - имя файлв
+     */
+    public void downloadImage(String src,String nameImage) throws IOException {
+        String path = PATH_IMAGE + "\\" + nameImage;
+        InputStream in;
+        URL connection = new URL(src);
+        HttpURLConnection urlConn;
+        try {
+            urlConn = (HttpURLConnection) connection.openConnection();
+            urlConn.setRequestMethod("GET");
+            urlConn.connect();
+            in = urlConn.getInputStream();
+            OutputStream writer = new FileOutputStream(path);
+            byte buffer[] = new byte[1];
+            int c = in.read(buffer);
+            while (c > 0) {
+                writer.write(buffer, 0, c);
+                c = in.read(buffer);
+            }
+            writer.flush();
+            writer.close();
+            in.close();
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+    }
+}
---
+++ src/ru/pochemuchki/musicproject/Song.java
@@ -0,0 +1,82 @@
+package ru.pochemuchki.musicproject;
+
+import javazoom.jl.decoder.JavaLayerException;
+import javazoom.jl.player.AudioDevice;
+import javazoom.jl.player.JavaSoundAudioDevice;
+import javazoom.jl.player.advanced.AdvancedPlayer;
+
+import java.io.*;
+import java.net.URL;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
+
+/**
+ * Класс для работы с музыкой.
+ * Класс скачивает музыку, воспроизводит музыку
+ */
+public class Song implements Constains{
+
+
+
+    /**
+     * Метод для скачивания музыки, получает ссылку на скачивание и название файла который скачивается
+     *
+     * @param src - сслыка на файл
+     * @param name        - имя файла
+     */
+    public void downloadSong(String src, String name) {
+        try {
+            download(src, name + ".mp3");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Воспроизводит музыку, на вход принимает имя файла
+     * @param nameSong - имя файла
+     */
+    public void playSong(String nameSong) {
+        AdvancedPlayer player;
+
+        try {
+            InputStream threath = new FileInputStream(nameSong);
+            AudioDevice auDev = new JavaSoundAudioDevice();
+
+            player = new AdvancedPlayer(threath, auDev);
+            player.play();
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Файл не найден");
+        } catch (JavaLayerException e) {
+            e.printStackTrace();
+        }
+
+    }
+    /**
+     * Метод для скачивание из интернета.
+     *
+     * @param src - ссылка на сайт от куда скачивать
+     * @param name   - имя которое будет у файла при создание
+     * @throws IOException
+     */
+    private static void download(String src, String name) throws IOException {
+        URL url = new URL(src);
+        FileOutputStream stream;
+
+        try (ReadableByteChannel byteChannelForDownload = Channels.newChannel(url.openStream())) {
+            stream = new FileOutputStream(name);
+            stream.getChannel().transferFrom(byteChannelForDownload, 0, Long.MAX_VALUE);
+        }
+
+        Path sound = Paths.get(name);
+        Files.copy(sound, Paths.get(PATH_MUSICS + "\\" + name), REPLACE_EXISTING);
+        Files.delete(sound);
+    }
+
+}
