7d9b8c58648a933828aae7c0e335f43a82f7c024
Dimut <Dimut2805@yandex.ru>
2019-12-01 16:37:07 +0300

    Некоторые изменения по GUI

--- src/ru/pochemuchki/musicproject/Constains.java
+++ src/ru/pochemuchki/musicproject/Constains.java
@@ -1,8 +1,21 @@
 package ru.pochemuchki.musicproject;
 
+import java.util.HashMap;
+
 public interface Constains {
     String DOWNLOAD_URL = "src\\downloadUrl.txt";
     String PATH_TO_MUSIC = "C:\\Users\\" + System.getProperty("user.name") + "\\Music\\"; // for use file in this directory
     String PATH_TO_IMAGE = "srs\\image";
     String PATH_MUSICS = "C:\\Users\\" + System.getProperty("user.name") + "\\Music";
+    HashMap<String, String>HASH_MAP_SECTIONS = new HashMap<String, String>() {
+        {
+            put("Не выбрано", null);
+            put("Топ-100", "https://muzika.vip");
+            put("Billie Eilish", "https://muzika.vip/m-artists/billie-eilish-121989");
+            put("Григорий Лепс", "https://muzika.vip/m-artists/григорий-лепс-6298");
+            put("Группа Кино", "https://muzika.vip/m-artists/виктор-цой-6257");
+            put("Макс Корж", "https://muzika.vip/m-artists/макс-корж-9229");
+            put("Lindsey Stirling", "https://muzika.vip/m-artists/lindsey-stirling-10942");
+        }
+    };
 }
--- src/ru/pochemuchki/musicproject/Controller.java
+++ src/ru/pochemuchki/musicproject/Controller.java
@@ -9,21 +9,9 @@ import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 
 
 public class Controller implements Constains {
-    public HashMap<String, String> hashMapNameKeySections = new HashMap<String, String>() {
-        {
-            put("Не выбрано", null);
-            put("Топ-100", "https://muzika.vip");
-            put("Billie Eilish", "https://muzika.vip/m-artists/billie-eilish-121989");
-            put("Григорий Лепс", "https://muzika.vip/m-artists/григорий-лепс-6298");
-            put("Группа Кино", "https://muzika.vip/m-artists/виктор-цой-6257");
-            put("Макс Корж", "https://muzika.vip/m-artists/макс-корж-9229");
-            put("Lindsey Stirling", "https://muzika.vip/m-artists/lindsey-stirling-10942");
-        }
-    };
     @FXML
     VBox window;
     @FXML
@@ -97,7 +85,7 @@ public class Controller implements Constains {
 
     @FXML
     private void getComboBox() {
-        urlSection = hashMapNameKeySections.get(sections.getValue());
+        urlSection = HASH_MAP_SECTIONS.get(sections.getValue());
     }
 
     @FXML
