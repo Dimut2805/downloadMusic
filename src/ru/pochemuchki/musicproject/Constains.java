7c6c6e01c3b9a585205c6ab541a97d9ad3971772
Dimut <Dimut2805@yandex.ru>
2019-12-02 12:05:08 +0300

    Убран мусор

--- src/ru/pochemuchki/musicproject/Constains.java
+++ src/ru/pochemuchki/musicproject/Constains.java
@@ -5,9 +5,6 @@ import java.util.HashMap;
 import java.util.HashMap;
 
 public interface Constains {
-    String DOWNLOAD_URL = "src\\downloadUrl.txt";
-    String PATH_TO_MUSIC = "C:\\Users\\" + System.getProperty("user.name") + "\\Music\\"; // for use file in this directory
-    String PATH_TO_IMAGE = "srs\\image";
     String PATH_MUSICS = "C:\\Users\\" + System.getProperty("user.name") + "\\Music";
     String PATH_IMAGE = "srs\\Users\\" + System.getProperty("user.name") + "\\Pictures";
 
